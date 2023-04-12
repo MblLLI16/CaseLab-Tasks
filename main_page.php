f7c250d9d8254c42f3fdfe251b26bcbb954ae5fb
mbllli16@mail.ru <mbllli16@mail.ru>
2023-04-12 12:23:19 +0800

    added back

--- main_page.php
+++ main_page.php
@@ -12,37 +12,76 @@
 </head>
 
 <body>
-    <div class="form">
-        <div class="heading">
-            <span><strong>Входные параметры системы</strong></span>
-        </div>
-        <div class="elements">
-            <div class="form-group">
-                <input type="text" class="form-control" id="eventName1" name="eventName1"
-                    placeholder="Интенсивность входного потока λ" required>
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" id="eventName2" name="eventName2"
-                    placeholder="Интенсивность обслуживания μ" required>
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" id="eventName3" name="eventName3" placeholder="Число каналов m"
-                    required>
-            </div>
-            <div class="form-group">
-                <input type="text" class="form-control" id="eventName4" name="eventName4" placeholder="Число мест ожидания k"
-                    required>
-            </div>
-        </div>
-        <div class="heading">
-            <span>_____</span>
-        </div>
+    <div class="container">
         <div class="row">
-            <button class="btn btn-primary mt-3" id="create-event-btn">Вычислить</button>
-        </div>
-    </div>
+            <div class="col-md-6">
+                <div class="form">
+                    <div class="heading">
+                        <span><strong>Входные параметры системы</strong></span>
+                    </div>
+                    <div class="elements">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="eventName1" name="eventName1"
+                                placeholder="Интенсивность входного потока λ" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="eventName2" name="eventName2"
+                                placeholder="Интенсивность обслуживания μ" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="eventName3" name="eventName3"
+                                placeholder="Число каналов m" required>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="eventName4" name="eventName4"
+                                placeholder="Число мест ожидания k" required>
+                        </div>
+                    </div>
+                    <div class="heading">
+                        <span>_____</span>
+                    </div>
+                    <div class="row">
+                        <button class="btn btn-primary mt-3" id="create-event-btn">Вычислить</button>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form">
+                    <div class="heading">
+                        <span><strong>Выходные параметры системы</strong></span>
+                    </div>
+                    <div class="elements">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="output1" name="output1"
+                                placeholder="Коэффициент загрузки системы ρ" readonly>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="output2" name="output2"
+                                placeholder="Среднее время ожидания в очереди W" readonly>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="output3" name="output3"
+                                placeholder="Длина очереди" readonly>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="output4" name="output4"
+                                placeholder="Количество заявок в системе" readonly>
+                        </div>
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="output5" name="output5"
+                                placeholder="Вероятность состояний p0,...,pk" readonly>
+                        </div>
+                    </div>
+                    <!-- <div class="form">
+                        <div class="heading">
+                            <span>График</span>
+                        </div>
+                        <div class="graph"></div>
+                    </div> -->
+                </div>
+            </div>
 
-    <script src="script.js"></script>
+            <script src="script.js"></script>
 
 </body>
 
--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-
 const eventName1 = document.getElementById("eventName1");
 const eventName2 = document.getElementById("eventName2");
-eventName2.addEventListener("keydown", (event) => {
-    if (eventName1.value && eventName2.value && event.keyCode === 13) {
-        window.location.href = "main_page.php";
+const eventName3 = document.getElementById("eventName3");
+const eventName4 = document.getElementById("eventName4");
+
+const output1 = document.getElementById("output1");
+
+document.getElementById("create-event-btn").addEventListener("click", (event) => {
+
+    if (eventName1.value && eventName2.value) {
+        const p = p_calculation(eventName1.value,eventName2.value);
+        output1.value = p.toFixed(2);
     }
 })
+
+
+
+//
+function p_calculation(L, u) {
+    let p = L / u;
+
+    return p;
+}
